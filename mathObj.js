45a6f10e65bcec7c7cb124542c4e953e5ab6b86a
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-08 17:03:41 +0630

    JsLesson24, 25

---
+++ mathObj.js
@@ -0,0 +1,12 @@
+// round(), ceil, floor, sqrt, abs, pow, min, max, random
+
+let num = 2.5;
+console.log(typeof num); // number
+console.log(Math.round(num)); // 3
+console.log(Math.ceil(num)); // 3
+console.log(Math.floor(num)); // 2
+console.log(Math.sqrt(4)); // 2
+console.log(Math.abs(-10)); // 10
+console.log(Math.pow(8, 2)); // 64
+console.log(Math.min(3, 5)); // 3
+console.log(Math.random()); // random values
---
+++ typeConverting.js
@@ -0,0 +1,20 @@
+// String(5), (true).toString(), Array to String
+// Date to String, using ().toString();
+// Number, NaN, ParserInt, ParseFloat, toFixed
+
+let n = 20;
+console.log(n); // 20
+console.log(typeof n); // number
+
+let str = String(n);
+let str2 = n.toString();
+console.log(typeof str); // string
+console.log(typeof str2); // string
+
+let st = "AA";
+let st1 = "20.6333333";
+console.log(parseInt(st1)); // 20
+console.log(Number(st1).toFixed(2)); // 20.63
+console.log(typeof Number(st)); // number
+console.log(Number(st)); // NaN
+console.log(Number(st1)); // 20.6333333
