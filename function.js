7c920cde192765eef1466cc2716457668f150f1b
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-08 17:20:51 +0630

    JsLesson27

---
+++ function.js
@@ -0,0 +1,9 @@
+// Functions
+// Function return Value
+// Function with Arguments
+function doIt(name, age) {
+  return "Name is " + name + " & Age is " + age;
+}
+
+let answer = doIt("Mg Mg", 20);
+console.log(answer); // Name is Mg Mg & Age is 20
