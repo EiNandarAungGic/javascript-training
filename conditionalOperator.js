4d82c41d0f3bdabbc1b77bbc31ebd48b098b56e8
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-08 15:27:29 +0630

    JsLesson16, 17, 18, 19

--- conditionalOperator.js
+++ conditionalOperator.js
@@ -70,3 +70,37 @@ switch (color) {
 }
 
 document.querySelector("#head").innerHTML = value;
+
+// Loop
+//  for loop
+for (let i = 0; i < 100; i++) {
+  console.log("i => " + i);
+}
+
+// while loop
+let j = 0;
+while (j < 5) {
+  console.log("j => " + j);
+  j++;
+}
+
+// do-while loop
+let k = 0;
+do {
+  console.log("k => " + k);
+  k++;
+} while (k < 3);
+
+let obj = [
+  {
+    name: "Mg Mg",
+  },
+  {
+    name: "AungAung",
+  },
+];
+let i = 0;
+while (i < obj.length) {
+  console.log(obj[i].name);
+  i++;
+}
