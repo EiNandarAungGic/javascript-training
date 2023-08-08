b8c935286fe9d8a3e0031d123f2406bfd697a10b
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-08 14:37:36 +0630

    JsLesson12

---
+++ conditionalOperator.js
@@ -0,0 +1,14 @@
+let x1 = 20;
+let y1 = 10;
+let k1 = "10";
+
+let z1 = x1 > y1;
+let z2 = x1 == y1;
+let z3 = x1 != y1;
+let z4 = y1 == k1;
+let z5 = y1 === k1; // === (type, value)
+console.log(z1); // true
+console.log(z2); // false
+console.log(z3); // true
+console.log(z4); // true
+console.log(z5); // false
