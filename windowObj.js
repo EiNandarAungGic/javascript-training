450e8e16464931840dff7bb6df760d866db6549e
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-09 09:55:12 +0630

    JsLesson35

---
+++ windowObj.js
@@ -0,0 +1,16 @@
+// Window Object
+let name = prompt("what's ur name?");
+document.querySelector("#head").innerHTML = name;
+
+let con = confirm("Is ur age over 16");
+let ans = con ? "Yes" : "No";
+document.querySelector("#head1").innerHTML = ans;
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+document.querySelector("#btn").addEventListener("click", function () {
+  //   window.location.reload();
+  //   window.location.href = "extra.html";
+  console.log(window.location.search);
+});
