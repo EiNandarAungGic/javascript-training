eb1ab15a0fb5a43191684b9c66c4df8250d1d14e
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-09 08:40:06 +0630

    JsLesson30

--- function.js
+++ function.js
@@ -38,3 +38,27 @@ let car_obj = {
 };
 
 car_obj.func();
+
+// ---------------------------------------------------------
+// Array Functions
+let ary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let ary2 = [100, 200];
+
+let bol = Array.isArray(ary);
+console.log(bol); // true
+console.log(ary.indexOf(1)); // 0
+console.log(ary); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+ary.push(100);
+console.log(ary); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 100]
+ary.unshift(200);
+console.log(ary); // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 100]
+ary.pop();
+console.log(ary); // [200, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+ary.shift();
+console.log(ary); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+ary.splice(2, 1);
+console.log(ary); // [1, 2, 4, 5, 6, 7, 8, 9, 0]
+ary.reverse();
+console.log(ary); // [0, 9, 8, 7, 6, 5, 4, 2, 1]
+let combine = ary.concat(ary2);
+console.log(combine); // [0, 9, 8, 7, 6, 5, 4, 2, 1, 100, 200]
