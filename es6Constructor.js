774b06b0d38f40333f613561c6ff6a99f9335360
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-10 16:47:38 +0630

    JsLesson52

--- es6Constructor.js
+++ es6Constructor.js
@@ -22,3 +22,29 @@ class Person {
 let person = new Person("mm", 20);
 console.log(person);
 console.log(Person.getNameAge());
+
+// ES6 Inheritance
+
+class Car {
+  constructor(name, speed) {
+    this.name = name;
+    this.speed = speed;
+  }
+  getSpeed() {
+    console.log(`This speed is ${this.speed}`);
+  }
+}
+
+class Toyota extends Car {
+  constructor(name, speed, price) {
+    super(name, speed);
+    this.price = price;
+  }
+  getSpeed() {
+    console.log("Override method of toyota");
+  }
+}
+
+let toyota = new Toyota("toyota", 180, 18000000);
+console.log(toyota.name);
+toyota.getSpeed();
