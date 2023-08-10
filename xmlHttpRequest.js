53cd96e123cf50402497e0e88fb1d84e828496d6
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-10 17:23:37 +0630

    JsLesson54

--- xmlHttpRequest.js
+++ xmlHttpRequest.js
@@ -1,22 +1,36 @@
 // Asynchronous JavaScript
+// onreadystatechange
+// onload
 
 let card = document.querySelector(".card");
 let url = "https://jsonplaceholder.typicode.com/posts";
 
 let xhr = new XMLHttpRequest();
 xhr.open("GET", url);
-xhr.onreadystatechange = function () {
-  if (this.status == 200 && this.readyState == 4) {
-    let res = JSON.parse(this.responseText);
-    let texts = "";
-    res.forEach((posts) => {
-      texts += `
+// xhr.onreadystatechange = function () {
+//   if (this.status == 200 && this.readyState == 4) {
+//     let res = JSON.parse(this.responseText);
+//     let texts = "";
+//     res.forEach((posts) => {
+//       texts += `
+//             <h4>${posts.title}</h4>
+//             <p>${posts.body}</p></br>
+//         `;
+//     });
+//     card.innerHTML = texts;
+//     console.log(this.responseText);
+//   }
+// };
+
+xhr.onload = function () {
+  let res = JSON.parse(this.responseText);
+  let texts = "";
+  res.forEach((posts) => {
+    texts += `
             <h4>${posts.title}</h4>
             <p>${posts.body}</p></br>
         `;
-    });
-    card.innerHTML = texts;
-    console.log(this.responseText);
-  }
+  });
+  card.innerHTML = texts;
 };
 xhr.send();
