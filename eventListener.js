375351e774272485e9dd9548f66f850323f57e0f
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-10 11:19:50 +0630

    JsLesson42

--- eventListener.js
+++ eventListener.js
@@ -3,6 +3,34 @@
 let btn = document.querySelector("#btn");
 let txt = document.querySelector("#txt");
 
-btn.addEventListener("click", function (e) {
-  txt.innerHTML = "You clicked button!";
+// btn.addEventListener("click", function (e) {
+//   txt.innerHTML = "You clicked button!";
+// });
+
+btn.addEventListener("dblclick", (e) => {
+  txt.innerHTML = "You clicked double!";
+});
+
+btn.onmouseup = (e) => {
+  txt.innerHTML = "You mouseup on button!";
+};
+
+btn.addEventListener("mousedown", (e) => {
+  txt.innerHTML = "You mousedown on button!";
+});
+
+btn.addEventListener("mouseenter", (e) => {
+  console.log("I'm mouseenter");
+});
+
+btn.addEventListener("mouseleave", (e) => {
+  console.log("I'm mouseleave");
+});
+
+btn.addEventListener("mouseover", (e) => {
+  console.log("I'm mouseover");
+});
+
+btn.addEventListener("mousemove", (e) => {
+  console.log("I'm mousemove");
 });
