1887ea1f3bc7f0102f9153bece338f4e6b315556
EiNandarAungGic <einandaraung@gicjp.com>
2023-08-10 12:56:01 +0630

    JsLesson44

--- eventListener.js
+++ eventListener.js
@@ -1,6 +1,7 @@
 // DOM => EventListener => onClick
 
 let btn = document.querySelector("#btn");
+let delBtn = document.querySelector("#delBtn");
 let txt = document.querySelector("#txt");
 
 // btn.addEventListener("click", function (e) {
@@ -39,13 +40,44 @@ btn.addEventListener("mousemove", (e) => {
 // keydown, keyup, keypress, focus, blur, cut, paste, change
 
 let form = document.querySelector("form");
-let input = document.querySelector("#inp");
+let input = document.querySelector("#name");
 
 input.addEventListener("cut", (e) => {
   txt.innerHTML = input.value;
 });
 
+// Local Storage
+
+let inputName = document.querySelector("#name");
+let inputAge = document.querySelector("#age");
+
+let addDataToDb = (obj) => {
+  let data = JSON.stringify(obj);
+  localStorage.setItem("localdb", data);
+};
+
+let getDataFromDb = () => {
+  let str = localStorage.getItem("localdb");
+  let obj = JSON.parse(str);
+  return obj;
+};
+
+let clearDb = () => {
+  localStorage.removeItem("localdb");
+};
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  txt.innerHTML = input.value;
+  // txt.innerHTML = input.value;
+  let name = inputName.value;
+  let age = inputAge.value;
+  let user = {
+    name: name,
+    age: age,
+  };
+  addDataToDb(user);
 });
+
+delBtn.onclick = (e) => {
+  clearDb();
+};
